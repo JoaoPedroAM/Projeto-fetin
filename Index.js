f1f52c7ed0cefdeae58e25ad71142ce522c93edd
Joao Pedro Andrade Moreira <joaoam@DESKTOP-9BI3QLA.localdomain>
2022-08-24 16:59:14 -0300

    changes

--- Index.js
+++ Index.js
@@ -3,8 +3,18 @@ const exphb = require("express-handlebars");
 const mysql = require("mysql");
 const port = 3000
 const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const initializePassport = require('./passport-config');
+const flash = require("express-flash");
+const session = require("express-session");
 const path = __dirname;
 
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
 app.engine("handlebars", exphb.engine());
 app.set("view engine", "handlebars");
@@ -14,13 +24,38 @@ app.use(
     extended: true
   }),
 )
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(express.json())
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/cadastro", (req, res) => {
-  res.render("cadastro");
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword =await bcrypt.hash(req.body.password, 10)
+    users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users);
+  
 });
 
 app.get("/login", (req, res) => {
